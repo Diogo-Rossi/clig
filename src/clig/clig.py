e01b93c8d37415e22f896981d40e9e74dc4c4a0e
Diogo Rossi <rossi.diogo@gmail.com>
2024-11-05 00:40:57 -0300

    🚧 wip: functional `run` without metadata

--- src/clig/clig.py
+++ src/clig/clig.py
@@ -1,27 +1,35 @@
 import inspect
+from inspect import signature, Parameter, Signature
 from argparse import ArgumentParser
 from typing import Callable, Any, Tuple
 
 
 def run(command: Callable[..., Any]):
     parser = ArgumentParser()
-    signature = inspect.signature(command)
+    signature: Signature = inspect.signature(command)
     parameters = signature.parameters
-    for par_name in parameters:
-        par = parameters[par_name]
-        par_type = str
-        par_default = None
-        nargs = None
-        if par.annotation != par.empty:
-            if hasattr(par.annotation, "__metadata__"):
-                print("TODO")
-                print(par.annotation.__origin__)
-                print(par.annotation.__metadata__)
-            if callable(par.annotation):
-                par_type = par.annotation
-        if par.default != par.empty:
-            nargs = "?"
-            par_default = par.default
-        parser.add_argument(par_name, default=par_default, nargs=nargs, type=par_type)
-    args = parser.parse_args()
-    command(**vars(args))
+    for name in parameters:
+        parameter: Parameter = parameters[name]
+
+        default = None
+        name = name.replace("_", "-")
+        if parameter.default != parameter.empty:
+            default = parameter.default
+            name = f"--{name}"
+        kwargs: dict[str, Any] = dict(type=str, action="store")
+
+        if parameter.annotation != parameter.empty:
+            if hasattr(parameter.annotation, "__metadata__"):
+                kwargs["type"] = parameter.annotation.__origin__
+                metadata = parameter.annotation.__metadata__
+            if callable(parameter.annotation):
+                kwargs["type"] = parameter.annotation
+
+        if kwargs.get("type", None) == bool:
+            kwargs["action"] = "store_false" if default else "store_true"
+
+        if kwargs.get("action", None) in ["store_const", "store_true", "store_false", "help"]:
+            kwargs.pop("type", None)
+
+        parser.add_argument(name, default=default, **kwargs)
+    command(**vars(parser.parse_args()))
